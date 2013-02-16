d9ec98b86aa1a206da8017e7ef92a3efcf0b1687
buckmarble <forrestmarble@gmail.com>
2013-02-16 13:59:36 -0500

    fix up inline kernel commit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,6 @@ BOARD_KERNEL_BASE := 0x80000000
 # BOARD_KERNEL_CMDLINE :=
 TARGET_KERNEL_CONFIG := ProjectElite_tuna_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/tuna
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro
 TARGET_PREBUILT_KERNEL := device/samsung/tuna/kernel
 
 TARGET_NO_RADIOIMAGE := true
